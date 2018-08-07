54cd41655577697e6fc763245de27dbe94d165f8
Gimo <self@gimo.me>
2018-08-07 11:33:19 +0900

    update polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+wlan=$(iw dev | awk '$1=="Interface"{print $2}')
+
 if type "xrandr"; then
   for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload bar &
+    MONITOR=$m WLAN=$wlan polybar --reload bar &
   done
 else
   polybar --reload bar &
