1e452e01a87ca646fbde743418da714a4a1bbe2f
Gimo <self@gimo.me>
2018-08-09 11:30:21 +0900

    update i3 config

--- .screenlayout/launch.sh
+++ .screenlayout/launch.sh
@@ -3,7 +3,7 @@
 host=$(hostname)
 
 if [ $host == 'air' ]; then
-  xrandr --output eDP1 --primary --scale 1.4x1.4 --below DP1 --output DP1 --mode 2048x1080
+  xrandr --output eDP1 --primary --scale 1.4x1.4 --below DP1 --output DP1 --mode 3840x2160
 elif [ $host == 'nexus' ]; then
   xrandr --output HDMI1 --primary --scale 1.57x1.57
 fi
