74133784eef61ada7a8a59bd62efd0a043dc9ce0
Gimo <self@gimo.me>
2018-09-02 17:23:14 +0800

    rename dir

---
+++ .auto_settings/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+host=$(hostname)
+
+if [ $host == 'air' ]; then
+  xrandr --output eDP1 --off --output DP1 --primary --mode 2560x1440 --scale 1.57x1.57
+elif [ $host == 'nexus' ]; then
+  xrandr --output HDMI1 --primary --scale 1.57x1.57
+elif [ $host == 'xps' ]; then
+  # DPI 192 / 240 = 0.8
+  xrandr --output eDP1 --primary --mode 3840x2160 --scale 0.8x0.8
+  xinput set-prop "DELL07E6:00 06CB:76AF Touchpad" 285 1
+  xinput set-prop "DELL07E6:00 06CB:76AF Touchpad" 293 1
+  libinput-gestures-setup restart
+fi
--- .screenlayout/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-host=$(hostname)
-
-if [ $host == 'air' ]; then
-  xrandr --output eDP1 --off --output DP1 --primary --mode 2560x1440 --scale 1.57x1.57
-elif [ $host == 'nexus' ]; then
-  xrandr --output HDMI1 --primary --scale 1.57x1.57
-fi
