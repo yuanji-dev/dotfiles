8e4b23154840ef476c4f39bef6cca978d6fc17b2
Gimo <self@gimo.me>
2018-12-04 15:34:53 +0900

    use subkey to sign commit

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,7 +2,9 @@ set -g theme_nerd_fonts yes
 set -g theme_color_scheme dracula
 set -gx EDITOR vim
 set -gx PATH $HOME/.local/bin $PATH
+set -gx GOPATH $HOME/Projects/gobook
 
 if status --is-interactive
-    keychain --eval --quiet -Q id_rsa | source
+    keychain --eval --quiet -Q id_ed25519 | source
+    keychain --eval --agents gpg --quiet -Q 3CB6EA7C55827AAD | source
 end
