e53128993a89677b4fa274dd31f854783832715d
Gimo <self@gimo.me>
2018-12-06 16:31:37 +0900

    use GNU stow to manage dotfiles

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+items=(
+    "ack"
+    "compton"
+    "fish"
+    "fontconfig"
+    "git"
+    "i3"
+    "i3blocks"
+    "libinput-gestures"
+    "misc"
+    "nvim"
+    "rofi"
+    "termite"
+)
+
+for item in "${items[@]}" ; do
+    stow -t $HOME -R ${item}
+    echo "${item} done."
+done
