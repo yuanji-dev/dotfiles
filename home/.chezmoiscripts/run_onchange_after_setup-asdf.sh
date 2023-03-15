e0c8d3bf431ad518f0a3b77b908c2f54c9d5679b
Yuanji <self@gimo.me>
2023-03-15 23:45:18 +0900

    Modified: home/.chezmoiscripts/run_onchange_after_setup-asdf.sh

--- home/.chezmoiscripts/run_onchange_after_setup-asdf.sh
+++ home/.chezmoiscripts/run_onchange_after_setup-asdf.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source "$HOME/.asdf/asdf.sh"
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add python https://github.com/asdf-community/asdf-python.git
 
