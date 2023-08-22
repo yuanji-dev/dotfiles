8f28c4cd19fab9e7e8f84547e086f1b21e76eec3
Yuanji <self@gimo.me>
2023-08-22 17:21:35 +0900

    Added: home/dot_vsnip/package.json Modified: home/dot_config/astronvim/lua/user/plugins/core.lua

--- home/dot_config/astronvim/lua/user/plugins/core.lua
+++ home/dot_config/astronvim/lua/user/plugins/core.lua
@@ -24,15 +24,22 @@ return {
   -- { "max397574/better-escape.nvim", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        -- this can be used if your configuration lives in ~/.config/nvim
+        -- if your configuration lives in ~/.config/astronvim, the full path
+        -- must be specified in the next line
+        paths = { "~/.vsnip" },
+      }
+      -- see https://github.com/rafamadriz/friendly-snippets/issues/327
+      require("luasnip").filetype_extend("sh", { "shelldoc" })
+    end,
+  },
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
