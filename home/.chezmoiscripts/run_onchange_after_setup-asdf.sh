96b5693c92857d6afd1e347aefa5ac0d92237644
Yuanji <yuanji@yuanji.dev>
2025-03-02 22:32:04 +0900

    feat: switch from asdf to mise

--- home/.chezmoiscripts/run_onchange_after_setup-asdf.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$HOME/.asdf/asdf.sh"
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add python https://github.com/asdf-community/asdf-python.git
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-
-asdf install
