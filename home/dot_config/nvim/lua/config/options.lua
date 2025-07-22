6ee949b4891a0478ff51be98dca7c0e9f93cad7e
Yuanji <yuanji@yuanji.dev>
2025-07-22 17:46:57 +0900

    Modified: home/dot_config/nvim/lazyvim.json

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+vim.g.lazyvim_ruby_lsp = "ruby_lsp"
+vim.g.lazyvim_ruby_formatter = "standardrb"
